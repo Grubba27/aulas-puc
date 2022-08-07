146e78e20b3a3db479f6503d0acf0cdf01365098
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-08-07 21:05:37 +0000

    feat(file): Organized index file

--- .build/index.js
+++ .build/index.js
@@ -17,27 +17,26 @@ var __toModule = (module2) => {
   return __reExport(__markAsModule(__defProp(module2 != null ? __create(__getProtoOf(module2)) : {}, "default", module2 && module2.__esModule && "default" in module2 ? { get: () => module2.default, enumerable: true } : { value: module2, enumerable: true })), module2);
 };
 var import_promises = __toModule(require("fs/promises"));
+const divider = (list, quantity) => list.reduce((accumulator, item, index) => {
+  const chunkIndex = Math.floor(index / quantity);
+  if (!accumulator[chunkIndex]) {
+    accumulator[chunkIndex] = [];
+  }
+  accumulator[chunkIndex].push(item);
+  return accumulator;
+}, []);
+const getUniqueItems = (conj1, conj2) => [...new Set(`${conj1},${conj2}`.split(","))];
+const getUnique = (conj) => [...new Set(conj.split(","))];
+const union = (conj1, conj2) => getUniqueItems(conj1, conj2).join(",");
+const intersection = (conj1, conj2) => getUniqueItems(conj1, conj2).filter((item) => conj1.includes(item) && conj2.includes(item)).join(",");
+const difference = (conj1, conj2) => getUniqueItems(conj1, conj2).filter((item) => !conj2.includes(item)).join(",");
+const cartesianProduct = (conj1, conj2) => getUnique(conj1).map((item1) => getUnique(conj2).map((item2) => `${item1},${item2}`));
+const format = (conj1, conj2) => (operation, result) => console.log(`${operation}: conjunto 1 {${conj1}}, conjunto 2 {${conj2}}. Resultado: {${result}} 
+`);
 async function main() {
   const FILE = await import_promises.default.readFile("./aula-01.txt", { encoding: "utf-8" });
-  const fileWithLines = FILE.split("\n");
-  const [numOps, ...file] = fileWithLines;
-  const divider = (list, quantity) => list.reduce((accumulator, item, index) => {
-    const chunkIndex = Math.floor(index / quantity);
-    if (!accumulator[chunkIndex]) {
-      accumulator[chunkIndex] = [];
-    }
-    accumulator[chunkIndex].push(item);
-    return accumulator;
-  }, []);
+  const [numOps, ...file] = FILE.split("\n");
   const divided = divider(file, 3);
-  const getUniqueItems = (conj1, conj2) => [...new Set(`${conj1},${conj2}`.split(","))];
-  const getUnique = (conj) => [...new Set(conj.split(","))];
-  const union = (conj1, conj2) => getUniqueItems(conj1, conj2).join(",");
-  const intersection = (conj1, conj2) => getUniqueItems(conj1, conj2).filter((item) => conj1.includes(item) && conj2.includes(item)).join(",");
-  const difference = (conj1, conj2) => getUniqueItems(conj1, conj2).filter((item) => !conj2.includes(item)).join(",");
-  const cartesianProduct = (conj1, conj2) => getUnique(conj1).map((item1) => getUnique(conj2).map((item2) => `${item1},${item2}`));
-  const format = (conj1, conj2) => (operation, result) => console.log(`${operation}: conjunto 1 {${conj1}}, conjunto 2 {${conj2}}. Resultado: {${result}} 
-`);
   Array.from(Array(Number(numOps)).keys()).map((item) => {
     const [op, conj1, conj2] = divided[item];
     const logger = format(conj1, conj2);
--- index.ts
+++ index.ts
@@ -1,11 +1,8 @@
 import fs from 'fs/promises';
-async function main () {
-  const FILE = await fs.readFile('./aula-01.txt', {encoding: 'utf-8'});
-  const fileWithLines = FILE.split("\n");
-  const [numOps, ...file] = fileWithLines;
 
-  type Divider = (list: string[], quantity: number) => string[][];
-  const divider: Divider = (list, quantity) =>
+type Divider = (list: string[], quantity: number) => string[][];
+const divider: Divider =
+  (list, quantity) =>
     list.reduce<string[][]>((accumulator, item, index) => {
       const chunkIndex = Math.floor(index / quantity)
 
@@ -17,45 +14,51 @@ async function main () {
       return accumulator
     }, []);
 
-  const divided = divider(file, 3);
-
-  type GetUniqueItems = (conj1: string, conj2: string) => string[];
-  const getUniqueItems: GetUniqueItems = (conj1, conj2) =>
+type GetUniqueItems = (conj1: string, conj2: string) => string[];
+const getUniqueItems: GetUniqueItems =
+  (conj1, conj2) =>
     [...new Set(`${conj1},${conj2}`.split(","))]
 
-  type GetUnique = (conj: string) => string[];
-  const getUnique: GetUnique = (conj) =>
-    [...new Set(conj.split(","))]
+type GetUnique = (conj: string) => string[];
+const getUnique: GetUnique =
+  (conj) => [...new Set(conj.split(","))]
 
-  type Union = (conj1: string, conj2: string) => string;
-  const union: Union = (conj1, conj2) =>
-    getUniqueItems(conj1, conj2)
-      .join(",");
+type Union = (conj1: string, conj2: string) => string;
+const union: Union =
+  (conj1, conj2) => getUniqueItems(conj1, conj2).join(",");
 
-  type Intersection = (conj1: string, conj2: string) => string;
-  const intersection: Intersection = (conj1, conj2) =>
+type Intersection = (conj1: string, conj2: string) => string;
+const intersection: Intersection =
+  (conj1, conj2) =>
     getUniqueItems(conj1, conj2)
       .filter(item => conj1.includes(item) && conj2.includes(item))
       .join(",");
 
-
-  type Difference = (conj1: string, conj2: string) => string;
-  const difference: Difference = (conj1, conj2) =>
+type Difference = (conj1: string, conj2: string) => string;
+const difference: Difference =
+  (conj1, conj2) =>
     getUniqueItems(conj1, conj2)
       .filter(item => !conj2.includes(item))
       .join(",");
 
-  type CartesianProduct = (conj1: string, conj2: string) => string[][];
-  const cartesianProduct: CartesianProduct = (conj1, conj2) =>
+type CartesianProduct = (conj1: string, conj2: string) => string[][];
+const cartesianProduct: CartesianProduct =
+  (conj1, conj2) =>
     getUnique(conj1)
       .map(item1 => getUnique(conj2)
         .map(item2 => `${item1},${item2}`))
 
-  const format =
-    (conj1: string, conj2: string) =>
-      (operation: string, result: string) =>
-        console.log(`${operation}: conjunto 1 {${conj1}}, conjunto 2 {${conj2}}. Resultado: {${result}} \n`);
+const format =
+  (conj1: string, conj2: string) =>
+    (operation: string, result: string) =>
+      console.log(`${operation}: conjunto 1 {${conj1}}, conjunto 2 {${conj2}}. Resultado: {${result}} \n`);
 
+async function main() {
+  const FILE = await fs.readFile('./aula-01.txt', {encoding: 'utf-8'});
+  
+  const [numOps, ...file] = FILE.split("\n");
+  
+  const divided = divider(file, 3);
   Array.from(Array(Number(numOps)).keys()).map((item) => {
     const [op, conj1, conj2] = divided[item];
     const logger = format(conj1, conj2);
